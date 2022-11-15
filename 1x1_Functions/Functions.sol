7dd65b6f9766725dae6e34aaae1d1e109750457e
Enes ÜNLÜ <enes9103@gmail.com>
2022-11-15 08:52:13 +0300

    feat: created modifier and visible functions

--- 1x1_Functions/Functions.sol
+++ 1x1_Functions/Functions.sol
@@ -32,4 +32,72 @@ contract Functions {
         z = false;
     }
 
+    // Fonksiyon Görünürlükleri
+
+    // - `public`: Herhangi bir akıllı kontrat ve hesap çağırabilir. 
+    // - `private`: Yalnızca fonksiyonun tanımlı olduğu kontratın içerisinde çağırılabilir. 
+    // - `internal`: Tanımlı olduğu kontrat ile birlikte miras olarak alındığı kontrat tarafından da çağırılabilir., 
+    // - `external`: Yalnızca diğer akıllı kontratlar ve hesaplar tarafından çağıralabilir. 
+
+    function add(uint a, uint b) public pure returns(uint) {
+        return a + b;
+    }
+
+    function add2(uint c, uint d) public pure returns(uint) {
+        return add(c, d);
+    }
+
+    // ---PUBLİC--- 
+    // Üstteki add fonksiyonunda yapılan a+ b işlemine add2 fonksiyonundan parametre gönderip erişerek işlem yaptık. Bu fonksiyon "public" olması nedeniyle bu fonksiyona erişip kullanabildik.
+
+    function publicKeyword() public pure returns(string memory) {
+        return "Bu bir public fonksiyondur.";
+    }
+
+    function callPublicKeyword() public pure returns(string memory) {
+        return publicKeyword();
+    }
+
+    // Üstteki fonksiyonlar da public olduğu için bir fonksyondaki string ifadeyi diğer fonksiyonlar callback yaparak okumuş olduk.
+
+    // --- PRİVATE ---
+
+    function privetKeyword() private pure returns(string memory) {
+        return "Bu bir private fonksiyondur.";
+    }
+
+    // Burada üstteki fonksiyon private olduğu için compile ettiğimizde bu fonksiyona ulaşamayız. ve okuyamayız. Bu fonksiyona sadece buradaki contract içinde ulaşılabilir.
+
+    function callPrivetKeyword() public pure returns(string memory) {
+        return privetKeyword();
+    }
+
+    // Ancak üstteki fonksiyon gibi public bir foknsiyon yazıp içinde private olan fonksiyonu çağırırsak bu fonksiyon (callPrivetKeyword()) public olduğu için privetKeyword() fonksiyonunu okuma yapabiliriz.
+
+
+    // --- INTERNAL ---
+
+    function internalKeyword() internal pure returns(string memory) {
+        return "Bu bir internal fonksiyondur.";
+    }
+
+    // Burada üstteki fonksiyon internal olduğu için compile ettiğimizde bu fonksiyona ulaşamayız. ve okuyamayız. Bu fonksiyona sadece buradaki contract içinde ulaşılabilir.
+
+    function callInternalKeyword() public pure returns(string memory) {
+        return internalKeyword();
+    }
+
+    // Ancak üstteki fonksiyon gibi public bir foksiyon yazıp içinde internal olan fonksiyonu çağırırsak bu fonksiyon (callInternalKeyword()) public olduğu için internalKeyword() fonksiyonunu okuma yapabiliriz. Private ile çok farkı yok gibi görünüyor ancak internal'ın mantığı Tanımlı olduğu kontrat ile birlikte miras olarak alındığı kontrat tarafından da çağırılabilir olmasıdır.
+
+    // --- EXTERNAL ---
+
+    function externalKeyword() external pure returns(string memory) {
+        return "Bu bir external fonksiyondur.";
+    }
+
+    // Burada üstteki fonksiyon external olduğu için compile ettiğimizde bu fonksiyona dışarıdan parametre göndererek direk ulaşabiliriz. Ancak alttaki gibi contract içinde kullanamayız. Altta contract içinde başka bir fonksiyon ile okuma yapmaya çalıştık ancak aynı contract içinde erişilemediği için hata verir.
+
+    function callExternalKeyword() public pure returns(string memory) {
+        return externalKeyword();
+    }
 }
